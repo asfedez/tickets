6a192d936d220ee3abbf4e85c420099e99b1fa3b
asfedez <solanofedezadrian@gmail.com>
2022-01-13 21:08:29 -0600

    Your awesome message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+
+import Home from './pages/Home';
+import Tickets from './pages/Tickets';
 
 function App() {
   return (
-    <div >
-      Hola
-    </div>
+    
+    <Router>
+      <nav>
+      <Link to="/">Home</Link>
+        <Link to="/tickets">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/tickets" element={<Tickets/>}></Route>
+      </Routes>
+    </Router>
+
   );
 }
 
---
+++ src/Pages/Home.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+function Home() {
+    return <div>This is the hOME</div>
+}
+
+export default Home;
