13d4696ba679470c416fcc59ac561bcbfafad5fc
asfedez <solanofedezadrian@gmail.com>
2022-01-13 22:11:58 -0600

    Your awesome message

--- src/Pages/Ticket.js
+++ src/Pages/Ticket.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
-import QRCode from "react-qr-code";
 
 function Ticket() {
 
@@ -9,7 +8,6 @@ function Ticket() {
     return(
         <div>
             This is the {ticketID} Ticket
-            <QRCode value={ticketID} size={256} bgColor="#282c34" fgColor="#fff" level="H" />
             <button
                 onClick={
                     () => {
