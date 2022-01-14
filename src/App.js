75b1c35951d9e1f445edb990ab32f92cad0e1696
asfedez <solanofedezadrian@gmail.com>
2022-01-14 12:47:20 -0600

    Your awesome message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 import Home from './Pages/Home';
 import Tickets from './Pages/Tickets';
@@ -7,16 +7,16 @@ import Ticket from './Pages/Ticket';
 
 function App() {
   return (
-    
+
     <Router>
       <nav>
-      <Link to="/">Home</Link>
+        <Link to="/">Home</Link>
         <Link to="/tickets">About</Link>
       </nav>
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/tickets" element={<Tickets/>}></Route>
-        <Route path="/ticket/:ticketID" element={<Ticket/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/tickets" element={<Tickets />}></Route>
+        <Route path="/ticket/:ticketID" element={<Ticket />}></Route>
       </Routes>
     </Router>
 
